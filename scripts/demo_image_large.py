ba53d11a17e89d9048ef8837f8be04725f7dc944
Dan Smith <dev@dansmith.tech>
2025-03-20 19:49:16 +0100

    Simplify project structure

--- scripts/demo_image_large.py
+++
@@ -1,9 +0,0 @@
-import time
-
-from filter.ml import filter_image
-
-if __name__ == "__main__":
-    start_time = time.process_time()
-    filter_image("media/demo-image-input-large.jpg", "media/demo-image-output-large.jpg", 3)
-    end_time = time.process_time()
-    print("Execution time:", end_time - start_time)
